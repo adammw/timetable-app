8f8ce71a34b8edc8c46b9b79125058d928197dc5
Adam Malcontenti-Wilson <adman.com@gmail.com>
2015-03-05 19:15:57 +1100

    Fix menu, add about page

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,6 @@
 Rails.application.routes.draw do
   root 'welcome#index'
+  get :about, to: 'welcome#about'
 
   resources :timetable, controller: :activity_allocation, only: [:new, :show, :create]
 end
